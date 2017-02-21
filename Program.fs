9dbf9db96f471b32d8cb285a980a8d42211713b3
Enrico Sada <enrico@sada.io>
2017-02-21 17:45:04 +0100

    initial commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let name = "Enrico"
+    let l = [1;2;3]
+    printfn "Hello World from F#!"
+    failwith "oh no!"
+    0 // return an integer exit code
